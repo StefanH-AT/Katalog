5bff606d7db93a11fdae8abcff9560bc7d9fcfa7
Stefan Heinz <stefan.heinz.02@gmail.com>
2024-12-24 01:41:40 +0100

    Delete dotnet project

--- Katalog.Web/Areas/Identity/Data/WebUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Katalog.Web.Areas.Identity.Data;
-
-// Add profile data for application users by adding properties to the WebUser class
-public class WebUser : IdentityUser
-{
-}
-
--- Katalog.Web/Services/IMediaStorage.cs
+++
@@ -1,9 +0,0 @@
-using Katalog.Web.Model;
-
-namespace Katalog.Web.Services;
-
-public interface IMediaStorage
-{
-    public string GetMediaUrl(Guid submissionPublicId);
-    public Task UploadMedia(Submission submission);
-}
