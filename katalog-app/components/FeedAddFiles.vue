be89b47dbc984fab83da46fadacd20a5ea16c28f
Stefan Heinz <stefan.heinz.02@gmail.com>
2025-01-01 01:03:23 +0100

    Make add file border hover effect

--- katalog-app/components/FeedAddFiles.vue
+++ katalog-app/components/FeedAddFiles.vue
@@ -2,7 +2,7 @@
   <UButton block
            :variant="isOverDropZone ? 'soft' : 'link'"
            :color="isOverDropZone ? 'primary' : 'neutral'"
-           class="border rounded-lg border-gray-700 h-full md:aspect-square gap-0.5 cursor-pointer text-lg"
+           class="border rounded-lg border-gray-700 hover:border-gray-400 h-full md:aspect-square gap-0.5 cursor-pointer text-lg"
            @click="() => open()">
 
     <UIcon name="lucide:plus" size="1.5em" v-if="!isOverDropZone"/>
