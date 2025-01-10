646659d9d3cfad80b1bbcfdc6de9eca2e58c4f63
Stefan Heinz <stefan.heinz.02@gmail.com>
2025-01-10 02:10:32 +0100

    Fix hydration mismatch on NuggetDetailShareButtons.vue

--- katalog-app/components/NuggetDetailShareButtons.vue
+++ katalog-app/components/NuggetDetailShareButtons.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
 
-import {useShare} from "@vueuse/core";
+import {useShare, type UseShareOptions} from "@vueuse/core";
 import type {NuggetMetaData} from "#shared/nugget/NuggetMetaData";
 
 const props = defineProps<{ nuggetMetaData: NuggetMetaData }>();
 
-const {share, isSupported} = useShare();
+const isSupportLoading = ref(true);
+const isShareSupported = ref(false);
+const shareFunction = ref<((overrideOptions?: (globalThis.MaybeRefOrGetter<UseShareOptions> | undefined)) => Promise<void>) | undefined>(undefined);
+
+// During SSR the server will always say it doesn't support share api so this creates a hydration mismatch. We also show the button as loading during hydration
+onMounted(() => {
+  const {share, isSupported} = useShare();
+  isShareSupported.value = isSupported.value;
+  shareFunction.value = share;
+  isSupportLoading.value = false;
+});
 
 function shareLink() {
-  share({
-    url: props.nuggetMetaData.image,
-    title: `Katalog - ${props.nuggetMetaData.nuggetFileName}`,
-  });
+  if(shareFunction.value) {
+    shareFunction.value({
+      url: props.nuggetMetaData.image,
+      title: `Katalog - ${props.nuggetMetaData.nuggetFileName}`,
+    });
+  }
 }
 
 async function shareFile() {
@@ -21,26 +33,24 @@ async function shareFile() {
       'Content-Type': 'application/octet-stream',
     },
   });
-  console.log(response);
 
   const blob = await response.blob();
-
-  console.log(blob);
 
   const file = new File([blob], props.nuggetMetaData.nuggetFileName, {
     type: blob.type,
   });
-  console.log(file);
 
-  share({
-    files: [file],
-    title: `Katalog - ${props.nuggetMetaData.nuggetFileName}`,
-  });
+  if(shareFunction.value) {
+    shareFunction.value({
+      files: [file],
+      title: `Katalog - ${props.nuggetMetaData.nuggetFileName}`,
+    });
+  }
 }
 
 </script>
 
 <template>
-  <UButton variant="subtle" color="secondary" label="Share Link" leading-icon="lucide:link" @click="shareLink" :disabled="!isSupported"/>
-  <UButton variant="subtle" color="secondary" label="Share File" leading-icon="lucide:image-up" @click="shareFile" :disabled="!isSupported"/>
+  <UButton variant="subtle" color="secondary" label="Share Link" leading-icon="lucide:link" @click="shareLink" :disabled="!isShareSupported" :loading="isSupportLoading"/>
+  <UButton variant="subtle" color="secondary" label="Share File" leading-icon="lucide:image-up" @click="shareFile" :disabled="!isShareSupported" :loading="isSupportLoading"/>
 </template>
